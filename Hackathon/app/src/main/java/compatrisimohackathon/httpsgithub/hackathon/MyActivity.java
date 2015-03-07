cbc1b7fdc6f3878e968dced06033f7b58c20dad0
patrisimo <patrickmartin@email.arizona.edu>
2015-03-07 15:08:29 -0700

    Have a page where users can input their names and take a selfie

--- Hackathon/app/src/main/java/compatrisimohackathon/httpsgithub/hackathon/MyActivity.java
+++ Hackathon/app/src/main/java/compatrisimohackathon/httpsgithub/hackathon/MyActivity.java
@@ -1,12 +1,22 @@
 package compatrisimohackathon.httpsgithub.hackathon;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 
 public class MyActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "compatrisimohackathon.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +45,43 @@ public class MyActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            Button pic_button = (Button) findViewById(R.id.pic_button);
+            pic_button.setVisibility(View.GONE);
+            ImageView pic_view = (ImageView) findViewById(R.id.selfie_disp);
+            pic_view.setImageBitmap(imageBitmap);
+            pic_view.setVisibility(View.VISIBLE);
+
+            Button rtk_button = (Button) findViewById(R.id.retake_button);
+            rtk_button.setVisibility(View.VISIBLE);
+
+        }
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    /** Called when the user clicks the "Take Picture" or "Retake Picture" button */
+    public void takePicture(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
     }
 }
